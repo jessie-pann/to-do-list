fc32606776ac241671f7741534002e943537d3a6
jessie pan <jessie.pan@autotrader.co.uk>
2024-07-09 10:42:38 +0100

    adding the basic elements to the app

---
+++ src/components/add-box.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const AddList = () => {
+  //   const [newList, setNewList] = useState("");
+
+  return (
+    <>
+      <input type="text" />
+      <button>Submit</button>
+    </>
+  );
+};
+
+export default AddList;
---
+++ src/components/lists.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ToDoList = () => {
+  return <p>this is the list</p>;
+};
+
+export default ToDoList;
